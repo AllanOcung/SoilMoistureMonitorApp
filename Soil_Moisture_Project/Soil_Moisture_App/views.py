9ac0a57cb61949e54672d72d2a46c18e0d8e2b2b
AllanOcung <allansocung137@gmail.com>
2024-07-15 13:29:26 +0300

    V1.2: Added user registration and login functionalities, and templates; registration.html and login.html

--- Soil_Moisture_Project/Soil_Moisture_App/views.py
+++ Soil_Moisture_Project/Soil_Moisture_App/views.py
@@ -1,14 +1,44 @@
-from django.shortcuts import render
-from django.contrib.auth import login
-from .forms import UserRegistrationForm, AdminRegistrationForm
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
+
+from django.contrib import messages
+
 
 def register(request):
+    form = RegisterForm()
+
     if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')
+            form.save()
+            user = form.cleaned_data.get('username')
+
+            messages.success(request, f'Registration successful! { user}, welcome to our community.')
+            return redirect('login')
     else:
-        form = UserRegistrationForm()
-    return render(request, 'registration/register.html', {'form': form})
+        form = RegisterForm()
+    return render(request, 'soil_moisture/register.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        # Implement authentication logic here
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'soil_moisture/login.html', {
+                'message': 'Invalid username or password'
+                })
+
+    return render(request, 'soil_moisture/login.html') 
+
+def home(request):
+    return render(request, 'soil_moisture/home.html')
--- Soil_Moisture_Project/Soil_Moisture_Project/settings.py
+++ Soil_Moisture_Project/Soil_Moisture_Project/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'Soil_Moisture_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'soil_moisture',
         'USER': 'root',
         'PASSWORD': '',
