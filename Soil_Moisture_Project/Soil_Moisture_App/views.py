5a619b4d898e2458050025018992103cc9592fa7
AllanOcung <allansocung137@gmail.com>
2024-07-16 19:44:08 +0300

    Finalized with the register and login, added the navbar for admin dasboard

--- Soil_Moisture_Project/Soil_Moisture_App/views.py
+++ Soil_Moisture_Project/Soil_Moisture_App/views.py
@@ -1,28 +1,38 @@
 
 from django.shortcuts import render, redirect
-from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from .forms import RegisterForm
+from django.contrib.auth.models import Group
 
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+from .decorators import *
 
 
+@unauthenticated_user
 def register(request):
     form = RegisterForm()
 
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
+            user = form.save()
+            username = form.cleaned_data.get('username')
 
-            messages.success(request, f'Registration successful! { user}, welcome to our community.')
+            group = Group.objects.get(name='farmers')
+
+            user.groups.add(group)
+            
+            messages.success(request, f'Registration successful! { username }, welcome to our community.')
             return redirect('login')
     else:
         form = RegisterForm()
     return render(request, 'soil_moisture/register.html', {'form': form})
 
 
+@unauthenticated_user
 def login(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -34,11 +44,20 @@ def login(request):
             auth_login(request, user)
             return redirect('home')
         else:
-            return render(request, 'soil_moisture/login.html', {
-                'message': 'Invalid username or password'
-                })
+            messages.info(request, 'username OR password is incorrect')
 
     return render(request, 'soil_moisture/login.html') 
 
+def logout(request):
+    auth_logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')  # This decorator makes sure the user is logged in before accessing the home page
+# @allowed_users(allowed_roles=['admin'])
+@admin_only
 def home(request):
     return render(request, 'soil_moisture/home.html')
+
+
+def Dashboard(request):
+    return render(request, 'soil_moisture/Dashboard.html')
--- Soil_Moisture_Project/Soil_Moisture_Project/urls.py
+++ Soil_Moisture_Project/Soil_Moisture_Project/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# Working with static files
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('Soil_Moisture_App.urls')),  # Include the Soil_Moisture_App URL configuration
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
