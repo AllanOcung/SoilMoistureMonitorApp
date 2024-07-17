c05726f98d20b85ca2ad3177f87046907f706f23
cissylyn <musiimentacissylyn7@gmail.com>
2024-07-17 17:40:23 +0300

    added charts

--- Soil_Moisture_Project/Soil_Moisture_App/views.py
+++ Soil_Moisture_Project/Soil_Moisture_App/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import login as auth_login, logout as auth_logout, auth
 from django.contrib.auth.forms import UserCreationForm
 from .forms import RegisterForm
 from django.contrib.auth.models import Group
+from .models import SoilData
+from django.db.models import Avg
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -60,5 +62,42 @@ def home(request):
     return render(request, 'soil_moisture/home.html')
 
 
-def Dashboard(request):
-    return render(request, 'soil_moisture/Dashboard.html')
+def dashboard_view(request):
+    # Fetch all entries
+    entries = SoilData.objects.all()
+    data = [
+        {
+            'date': entry.date.strftime('%Y-%m-%d'),
+            'time': entry.time.strftime('%H:%M:%S'),
+            'soil_moisture': entry.soil_moisture,
+            'temperature': entry.temperature,
+            'humidity': entry.humidity,
+            'rainfall': entry.rainfall
+        }
+        for entry in entries
+    ]
+    
+    # Calculate average soil moisture per soil texture
+    soil_texture_data = SoilData.objects.values('soil_texture').annotate(avg_moisture=Avg('soil_moisture'))
+    
+    # Calculate average rainfall per area
+    average_rainfall_per_area = SoilData.objects.values('location').annotate(avg_rainfall=Avg('rainfall'))
+
+    # Transform soil_texture_data for horizontal display
+    soil_texture_headers = [item['soil_texture'] for item in soil_texture_data]
+    avg_moistures = [item['avg_moisture'] for item in soil_texture_data]
+     # Calculate average soil moisture and average rainfall per location
+    location_data = SoilData.objects.values('location').annotate(
+        avg_moisture=Avg('soil_moisture'),
+        avg_rainfall=Avg('rainfall')
+    )
+
+    context = {
+        'data': data,
+        'soil_texture_headers': soil_texture_headers,
+        'avg_moistures': avg_moistures,
+        'average_rainfall_per_area': list(average_rainfall_per_area),
+        'location_data': location_data
+    }
+    
+    return render(request, 'soil_moisture/Dashboard.html', context)
