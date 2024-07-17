8ae4a2578a8bfa99e9a9d53c7136c5f0fcd386cf
cissylyn <musiimentacissylyn7@gmail.com>
2024-07-17 18:53:51 +0300

    added charts

--- Soil_Moisture_Project/Soil_Moisture_App/urls.py
+++ Soil_Moisture_Project/Soil_Moisture_App/urls.py
@@ -8,9 +8,7 @@ urlpatterns = [
     
         path('', views.register, name='register'),
         path('login/', views.login, name='login'),
-         
-          #path('dashboard/', views.soil_texture_moisture_view, name='dashboard'),
         path('logout/', views.logout, name='logout'),
         path('home/', views.home, name='home'),
-        path('dashboard/', views.dashboard_view, name='dashboard'),
+        path('dashboard/', views.dashboard, name='dashboard'),
    ]
